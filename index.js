256a3c100ab4ea34b707c669f31f0130ebacab59
César Adrián Reséndiz <cesaradrianer@gmail.com>
2023-12-18 23:04:24 -0600

    Basic operations added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const numbers = document.getElementsByClassName('number')
-const operations = document.getElementsByClassName('operations')
+const operations = document.getElementsByClassName('operation')
 const input = document.getElementById('input')
 const equals = document.getElementById('equals')
 const clear = document.getElementById('clear')
+const inputDiv = document.getElementById('input_wrapper')
 
 for (let i = 0; i < numbers.length; i++) {
 
@@ -14,40 +15,40 @@ for (let i = 0; i < numbers.length; i++) {
 
 }
 
+let firstValue
+let operator
+let secondValue
+
+for (let j = 0; j < operations.length; j++) {
+
+    operations[j].addEventListener('click', () => {
+
+        firstValue = parseInt(input.value)
+        operator = operations[j].innerHTML
+        input.value = ""
+
+    })
+
+}
+
+equals.addEventListener('click', () => {
+    secondValue = parseInt(input.value)
+    input.value = operate(firstValue, operator, secondValue)
+})
+
+clear.onclick = () => {input.value = ""}
+
 function operate(num1, operator, num2) {
 
     switch(operator) {
         case '+':
-            input.value = num1 + num2
+            return num1 + num2
         case '-':
-            input.value = num1 - num2
+            return num1 - num2
         case '*':
-            input.value = num1 * num2
+            return num1 * num2
         case '/':
-            input.value = num1 / num2
+            return num1 / num2
     }
-
-}
-
-let firstNumber
-let secondNumber
-let operator
-
-function setNum1() {
-    firstNumber = input.value
-}
-
-function setNum2() {
-    secondNumber = input.value
-}
-
-for (let j = 0; j < operations.length; j++) {
-
-    operations[i].addEventListener('click', () => {
-
-        input.value = ''
-        operator = operations[i].value
-
-    })
 
 }
