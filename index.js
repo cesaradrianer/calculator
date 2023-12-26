44ea6e8b934e6a4f937424eb4ed56d7853023aa9
cesaradrianer <cesaradrianer2@gmail.com>
2023-12-26 00:16:23 -0600

    Added a flag varible to determine the right moment to start writing to the input screen

--- index.js
+++ index.js
@@ -13,8 +13,16 @@ for (let i = 0; i < numbers.length; i++) {
 
     numbers[i].addEventListener('click', () => {
 
-        input.value += numbers[i].value
+        if (flag == true) {
 
+            input.value += numbers[i].value
+
+        } else {
+
+            return null
+
+        }
+        
     })
 
 }
@@ -23,6 +31,7 @@ for (let i = 0; i < numbers.length; i++) {
 let firstValue
 let operator
 let secondValue
+let flag = true
 
 /* First, the user has to input a first value to be worked with */
 /* Next, the user has to choose an operation, when the user clicks on any operation button, an event will trigger that defines 'firstValue' as the numbers written on our input, and also defines 'operator' as the chosen operation */
@@ -30,9 +39,17 @@ for (let j = 0; j < operations.length; j++) {
 
     operations[j].addEventListener('click', () => {
 
-        firstValue = parseFloat(input.value)
-        operator = operations[j].innerHTML
-        input.value = ""
+        if (flag == true) {
+
+           firstValue = parseFloat(input.value)
+           operator = operations[j].innerHTML
+           input.value = ""
+
+        } else {
+
+            return null
+
+        }
 
     })
 
@@ -41,12 +58,23 @@ for (let j = 0; j < operations.length; j++) {
 /* Second, the user now has to write a second value for the operation to work with */
 /* And when the user clicks on the equals button, another event triggers, now this time 'secondValue' gets defined as the numbers written on the input, and finally the 'operate' function executes and writes the result inside our input */
 equals.addEventListener('click', () => {
-    secondValue = parseFloat(input.value)
-    input.value = operate(firstValue, operator, secondValue)
+
+    if (flag == true) {
+
+        secondValue = parseFloat(input.value)
+        input.value = operate(firstValue, operator, secondValue)
+
+    } else {
+
+        return null
+
+    }
+    
 })
 
 /* When the user clicks on the 'CE' button, an event defines the value of our input as an empty string, and also defines our working variables as null */
 clear.onclick = () => {
+    flag = true
     input.value = ""
     num1 = null
     num2 = null
@@ -55,15 +83,26 @@ clear.onclick = () => {
 
 /* When the user clicks on the 'DEL' button, an event takes the text inside our input, creates an array from it, and removes the last element from it, only to then redefine the text inside our input as the modified array*/
 deleteBtn.onclick = () => {
-    let inputText = Array.from(input.value)
-    inputText.pop()
-    input.value = inputText.join('')
+
+    if (flag == true) {
+
+        let inputText = Array.from(input.value)
+        inputText.pop()
+        input.value = inputText.join('')
+
+    } else {
+
+        return null
+
+    }
+
 }
 
 /* This function takes all the necessary data and first checks if all variables aren't empty, if nothing goes wrong then a switch statement checks for the operation type, and performs the operation */
 function operate(num1, operator, num2) {
 
     if (num1 == undefined || num2 == undefined) {
+        flag = false
         return "INVALID OPERATION"
     }
 
@@ -75,7 +114,12 @@ function operate(num1, operator, num2) {
         case '*':
             return num1 * num2
         case '/':
-            return num1 / num2
+            if (num2 == 0) {
+                flag = false
+                return "Wait, are you crazy?"
+            } else {
+                return num1 / num2
+            }
     }
 
 }
